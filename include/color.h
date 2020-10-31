75fedfb9c8f7d9d3c053c828dfc7059a78663d4d
CohenArthur <arthur.cohen@epita.fr>
2020-10-31 15:53:35 +0100

    log: Add logging library from ubsh

---
+++ include/color.h
@@ -0,0 +1,23 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+/**
+ * @file color.h Defines helper macros around color gestion
+ *
+ * The colors should stay within the 8-16 bit color range for compatibility with
+ * most terminals
+ */
+
+#define RESET "\x1B[0m"
+
+/* Font color */
+#define RED(__S) "\x1B[31m" __S RESET
+#define GREEN(__S) "\x1B[32m" __S RESET
+#define YELLOW(__S) "\x1B[33m" __S RESET
+#define BLUE(__S) "\x1B[34m" __S RESET
+#define MAGENTA(__S) "\x1B[35m" __S RESET
+
+/* Highlight color */
+#define RED_HI(__S) "\x1B[101m" __S RESET
+
+#endif /* ! COLOR_H */
